229a3bd32fc0f414874f8752a07ba2549f98c1ef
angelicrf <angelicrf@gmail.com>
2020-04-09 09:57:43 -0700

    WPart1

--- program-isit420/420Orders/public/javascripts/index.js
+++ program-isit420/420Orders/public/javascripts/index.js
@@ -1,42 +1,44 @@
 
-
+let infos = {
+    storeNumber:  [98053 , 98007, 98077, 98055, 98011, 98046],
+    salesPersonID: [1,2,3,4,5,6,7,8,9,10,
+                    11,12,13,14,15,16,17,
+                    18,19,20,21,22,23,24],
+    itemNumber: [123456, 123654, 321456, 321654, 654123,
+                 654321, 543216, 354126, 621453, 623451],
+    timePurch: Date(Date.now()),
+    pricePaid: [5,6,7,8,9,10,11,12,13,14,15]
+ 
+}
+let myRand = infos.storeNumber[Math.floor(Math.random() * infos.storeNumber.length)];
+let myRand_2 = infos.salesPersonID[Math.floor(Math.random() * infos.salesPersonID.length)];
+let myRand_3 = infos.itemNumber[Math.floor(Math.random() * infos.itemNumber.length)];
+let myRand_4 = infos.pricePaid[Math.floor(Math.random() * infos.pricePaid.length)];
+let newObject = {
+       storeNumber:  myRand,
+       salesPersonID: myRand_2,
+       itemNumber: myRand_3,
+       timePurch: Date(Date.now).toString(),
+       pricePaid: myRand_4
+ }
 document.addEventListener("DOMContentLoaded", function(event){
+
     document.getElementById('button1').addEventListener("click", function(){
-        document.getElementById('textbox1').value = 'done';
+            document.getElementById('show').innerHTML += myRand + ' ' + myRand_2 + ' ' + myRand_3 
+             + ' ' + myRand_4 + ' ' + infos.timePurch.toString();   
     })
 })
 
-let infos = {
-    firstName : "Angelique",
-    lastName: "Refahiyat"
-
-}
-
 document.getElementById('button2').addEventListener("click", async function(){
   
-    let myVar = {
-        firstName: 'Angeliique',
-        lastName: 'Refahiyat'
-    }
-    fetch("http://localhost:3000/newOrder", {
-        method: "POST",
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(myVar)
-        //body: {"id" : document.getElementById('saada').value}
-    }).
-    then(response => {
-        if (response.ok) {
-            response.json()
-            .then(json => {
-              console.log(json);
-
-        document.getElementById('show').innerHTML = 'Angelique'
-            })
+    $.ajax({
+        url: '/newOrder/',
+        method: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(newObject),
+        success: function (result) {
+           
         }
-       
-    });
-      
-})
+
+    })  })      
--- program-isit420/420Orders/routes/index.js
+++ program-isit420/420Orders/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+//app.use(express.json());
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index.html');
 });
-router.get('/newOrder', function(req, res){
-  res.send('Here is the first submit');
+router.post('/newOrder', function(req, res){
+  console.log(req.body);
 })
 
 module.exports = router;
